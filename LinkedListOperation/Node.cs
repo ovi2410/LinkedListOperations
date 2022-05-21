5918471e988a5d4542976c7269dff2cd10db323f
ovi2410 <guneshwarifegade@gmail.com>
2022-05-21 06:03:47 +0530

    insert middle

--- LinkedListOperation/Node.cs
+++ LinkedListOperation/Node.cs
@@ -12,9 +12,7 @@ namespace LinkedListOperation
 
 
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public Node(int d)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
             data = d;
             next = null;
