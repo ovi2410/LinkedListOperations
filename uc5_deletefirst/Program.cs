e2e313949677af6d5bd8c5dc6dee401b0b43902e
ovi2410 <guneshwarifegade@gmail.com>
2022-05-25 16:14:22 +0530

    [ovi]delete first

---
+++ uc5_deletefirst/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using uc5_deletefirst;
+
+namespace LinkedListOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****WELCOME TO LINKED LIST OPERATIONS");
+            Console.WriteLine("**********1.Inserted into list(From Last)************");
+            Console.WriteLine("**********2.Inserted into list(Front)************");
+            Console.WriteLine("**********3.Inserting in middle***********");
+            Console.WriteLine("**********4.Delete the First Element***********");
+            Console.WriteLine("**********5.Delete the Last Element***********");
+            Console.WriteLine("**********6.Searching the Element***********");
+            Console.WriteLine("Enter the option!!!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //Creating object for Custom Linked list class
+            Operations obj = new uc5_deletefirst.Operations();
+            switch (num)
+            {
+                case 1:
+                    obj.InsertLast(56);
+                    obj.InsertLast(30);
+                    obj.InsertLast(70);
+                    obj.Display();
+                    break;
+                case 2:
+                    obj.InsertFront(56);
+                    obj.InsertFront(30);
+                    obj.InsertFront(70);
+                    obj.Display();
+                    break;
+                case 3:
+                    obj.InsertLast(56);
+                    obj.InsertLast(70);
+                    obj.InsertBetween(2, 30);
+                    obj.Display();
+                    break;
+                case 4:
+                    obj.InsertFront(56);
+                    obj.InsertFront(30);
+                    obj.InsertFront(70);
+                    obj.DeleteFirst();
+                    obj.Display();
+                    break;
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
